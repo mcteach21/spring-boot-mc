c5a47be56b8ca394523f7b60b402a4cc9f7f523a
MC-Teacher <mc.teach.21@gmail.com>
2021-12-20 11:34:21 +0100

    default view

--- src/main/java/mc/apps/main/controller/MainController.java
+++ src/main/java/mc/apps/main/controller/MainController.java
@@ -8,6 +8,11 @@ import org.springframework.web.bind.annotation.PathVariable;
 public class MainController {
     private String DEFAUlT_VIEW="index";
 
+    @GetMapping("/")
+    public String index(){
+        return DEFAUlT_VIEW;
+    }
+
     @GetMapping("/{page}")
     public String display(@PathVariable() String page ){
         return "".equals(page)?DEFAUlT_VIEW:page;
